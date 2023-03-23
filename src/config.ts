6aae5a578a8301eae105f40202a41e32fd5da882
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2023-03-24 01:25:29 +0900

    brand

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+const config = {
+  siteUrl: 'https://kohatabe.jp',
+  copyright: {
+    year: 2021,
+    author: 'Nectarition'
+  },
+  event: {
+    name: '小春六花はヨーグルトが食べたい',
+    date: '2023-06-18',
+    place: '池袋サンシャインシティ',
+    keyVisualAuthor: '佐野はじめ',
+    content: '小春六花プチオンリーイベント',
+    twitterId: 'nct_kohatabe'
+  }
+}
+
+export default config
