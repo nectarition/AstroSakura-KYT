fe7e5df3b9729a25199f56b095b3a1b6feeda3b6
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2023-03-24 01:34:24 +0900

    キービジュアル消す

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,6 @@ const config = {
     name: '小春六花はヨーグルトが食べたい',
     date: '2023-06-18',
     place: '池袋サンシャインシティ',
-    keyVisualAuthor: '佐野はじめ',
     content: '小春六花プチオンリーイベント',
     twitterId: 'nct_kohatabe'
   }
