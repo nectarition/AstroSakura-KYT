43df970e7ae648982227b91e04b1dc5e6793159b
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2024-09-03 17:19:56 +0900

    SZP25

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 const config = {
   siteUrl: 'https://kohatabe.jp',
   copyright: {
-    year: 2021,
+    year: 2024,
     author: 'ねくたりしょん'
   },
   event: {
     name: '小春六花はヨーグルトが食べたい',
-    date: '2024-07-21',
-    place: '名古屋・ポートメッセなごや',
+    date: '2025-05-04',
+    place: '東京・大田区産業プラザPiO',
     content: '小春六花オンリー同人誌即売会',
     twitterId: 'nct_kohatabe'
   },
